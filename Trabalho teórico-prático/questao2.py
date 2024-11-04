f5f01944d36f68ef73c9f326a12bf9c20142abfe
João Victor <jvcgomes14@gmail.com>
2024-11-04 10:04:56 -0300

    testes extras na questao 2

--- Trabalho teórico-prático/questao2.py
+++ Trabalho teórico-prático/questao2.py
@@ -1,4 +1,8 @@
 import re
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
 
 # Expressão regular final da questão anterior
 expressao_regular = r"(0|-?[1-9][0-9]*)|(((0|[1-9][0-9]*):(0|[1-9][0-9]*))|((-[1-9][0-9]*):(-[1-9][0-9]*)))|(\"[\w\s]+\"|'[\w\s]+')|((\"[\w\s]+\"|'[\w\s]+'):(\"[\w\s]+\"|'[\w\s]+'))"
@@ -7,8 +11,9 @@ expressao_regular = r"(0|-?[1-9][0-9]*)|(((0|[1-9][0-9]*):(0|[1-9][0-9]*))|((-[1
 cadeias = [
     "0", "10", "-2", "-0",                                          
     "0:5", "2:2", "-1:-5", "0:-5", "-5:0", "-2:6", "6:-2",                    
-    "'Date'", '"New Column"', "'Date"+'"', '"Date'+"'", "'New Column"+'"', '"New Column'+"'",                                     
-    "'Data':'State'", '"District":"Tested"', "'simples'"+":"+'"duplas"', '"duplas"'+":"+"'simples'",                           
+    "'Date'", '"New Column"', "'valor1'", '"valor2"', "'Date"+'"', '"Date'+"'", "'New Column"+'"', '"New Column'+"'",                                     
+    "'Data':'State'", '"District":"Tested"', "'simples'"+":"+'"duplas"', '"duplas"'+":"+"'simples'", 
+    '"aspas_diferentes'+"'"+":"+"'simples'", "'simples'"+":"+'"aspas_diferentes'+"'", '"aspas_diferentes'+"'"+":"+'"duplas"', '"duplas"'+":"+'"aspas_diferentes'+"'"                        
 ]
 
 # Função para testar as cadeias
@@ -18,9 +23,9 @@ def testar_expressao(expressao, testes):
     for teste in testes:
         match = padrao.fullmatch(teste)
         if match:
-            resultados.append(f"{teste} ---> é reconhecida")
+            resultados.append(f"{Fore.GREEN}{teste} é reconhecida{Style.RESET_ALL}")
         else:
-            resultados.append(f"{teste} ---> NÃO é reconhecida")
+            resultados.append(f"{Fore.RED}{teste} não é reconhecida{Style.RESET_ALL}")
     return resultados
 
 # Executando o teste
